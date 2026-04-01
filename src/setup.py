3c148b9e1ce37cb970893e541290c7478edc1421
atvcaptain <openatv@gmail.com>
2026-04-01 07:00:24 +0200

    update setup.py move data parts to bb

--- src/setup.py
+++ src/setup.py
@@ -7,6 +7,6 @@ setup(name='enigma2-plugin-systemplugins-bluetoothsetup',
        description='VuPLus bluetooth plugin',
        package_dir={pkg: 'VuBluetoothSetup'},
        packages=[pkg],
-       package_data={pkg: ['images/*.png', '*.png', '*.xml', 'locale/*/LC_MESSAGES/*.mo', 'plugin.png', 'bt_audio.png', 'bt_keyboard.png', 'bt_misc.png', 'bt_rc.png', 'keymap.xml', 'vu_rcu_firmware.bin', 'vuduo4k/_vubt.so', 'vuduo4kse/_vubt.so', 'vusolo4k/_vubt.so', 'vuultimo4k/_vubt.so', 'vuuno4k/_vubt.so', 'vuuno4kse/_vubt.so', 'vuzero4k/_vubt.so']},
+       package_data={pkg: ['images/*.png', '*.png', '*.xml', 'locale/*/LC_MESSAGES/*.mo', 'plugin.png', 'bt_audio.png', 'bt_keyboard.png', 'bt_misc.png', 'bt_rc.png', 'keymap.xml', 'vu_rcu_firmware.bin']},
        cmdclass=setup_translate.cmdclass,  # for translation
       )
